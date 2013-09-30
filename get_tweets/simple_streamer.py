8dd9f7c665987f859f0903ffb502b97b41d57e9f
Konrad Komorowski <konrad@komorowski.me>
2013-09-30 15:48:22 +0200

    Add a simple streamer which outputs all valid Tweets and disconnects on an error.

---
+++ get_tweets/simple_streamer.py
@@ -0,0 +1,19 @@
+from twython import TwythonStreamer
+
+from twitter_authentication import TwitterAuthentication
+
+class SimpleStreamer(TwythonStreamer):
+    """A simple Twitter stream reading class used for debug purposes."""
+
+    def on_success(self, data):
+        if 'text' in data:
+            print('{id}: {text}'.format(id=data['id'], text=data['text']))
+    
+    def on_error(self, status_code, data):
+        print(data)
+        self.disconnect()
+
+if __name__ == '__main__':
+    stream = SimpleStreamer(*TwitterAuthentication().get_credentials())
+    stream.statuses.sample(language='en')
+    
