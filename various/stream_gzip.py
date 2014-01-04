fd6c071fa2369e662a6e3515a8f5185c82d63d7e
Konrad Komorowski <5056119+kkom@users.noreply.github.com>
2014-01-04 20:29:47 +0100

    Add a script to stream lines over HTTP from a gzipped file.

---
+++ various/stream_gzip.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+descr = """
+This script streams a gzip-compressed file from a network location.
+"""
+
+import argparse
+import gzip
+import itertools
+import time
+import urllib.request
+
+# Define and parse the script arguments
+parser = argparse.ArgumentParser(
+    description=descr,
+    formatter_class=argparse.RawDescriptionHelpFormatter
+)
+
+parser.add_argument("URL", help="web address of the file")
+
+parser.add_argument("--start", type=int, default=0,
+    help="skip lines until this one")
+parser.add_argument("--stop", type=int, default=None,
+    help="do not show lines beyond this one")
+parser.add_argument("--step", type=int, default=1,
+    help="step over that many lines")
+
+args = parser.parse_args()
+
+# Define iterators used in the script
+def iter_remote_gzip(url):
+    with urllib.request.urlopen(url) as f:
+        with gzip.GzipFile(fileobj=f) as g:
+            for l in g:
+                yield l
+
+line_numbers = map(lambda x: args.step*x, itertools.count())
+lines = itertools.islice(
+    iter_remote_gzip(args.URL), args.start, args.stop, args.step
+)
+
+# Run and time the iterators
+start = time.time()
+for (i,l) in zip(line_numbers, lines):
+    print("{}: {}".format(i, l.decode()), end="")
+interval = time.time() - start
+        
+print('The request took {:.2f} s of real time.'.format(interval))
