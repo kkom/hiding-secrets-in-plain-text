c135e59ffeb934e55c7130cdf8dc6fce995550fd
Konrad Komorowski <konrad@komorowski.me>
2014-01-26 13:28:09 +0000

    Change the way ngrams are translated from words to indices. If an ngram from a file assigned to partition A starts with a word from partition B (as defined by its 1gram) and A != B, discard the ngram. This can be done much cleaner in the future -- by either merging the two problematic partitions, or outputting the ill-defined ngrams to the correct partition.

--- scripts/googlebooks_ngrams/translate_googlebooks_ngrams_to_indices.py
+++ scripts/googlebooks_ngrams/translate_googlebooks_ngrams_to_indices.py
@@ -9,10 +9,13 @@ corresponding word, separated by a tab.
 """
 
 import argparse
+import functools
+import json
 import multiprocessing
 import os
 
 from pysteg.common.files import open_file_to_process, FileAlreadyProcessed
+from pysteg.googlebooks import get_partition
 from pysteg.googlebooks_ngrams.ngrams_analysis import gen_ngram_descriptions
 
 def read_index(index_file):
@@ -33,26 +36,44 @@ def process_file(descr):
     input_path = os.path.join(args.input, filename)
     output_path = os.path.join(args.output, filename)
     
+    partition = get_partition(prefix, partitions)
+    
+    unindexed = 0
+    bad_partition = 0
+    
     with open(input_path, "r") as i:
         with open_file_to_process(output_path, "w") as o:
             if o == False:
                 raise FileAlreadyProcessed()
-                
-            print("Translating from {input_path}".format(**locals()))
             
             for line in i:
                 try:
                     l = line.split("\t")
                     l[:-1] = [index[w] for w in l[:-1]]
+                    
+                    # Check if the first word of the ngram satisfies partition
+                    # index constraint 
+                    w1 = int(l[0])
+                    if (w1 < index_ranges[partition][0]
+                            or w1 > index_ranges[partition][1]):
+                        print("{l[0]} does not belong to {partition}".format(
+                            **locals()))
+                        bad_partition += 1
+                        continue
+                    
                     o.write("\t".join(l))
                 except KeyError:
                     # If some word is not in the index (there are only about 10
                     # such words), do not save the ngram. The distribution is
                     # not distorted very much, but it is much easier to
                     # construct the index.
-                    pass
+                    print("Unindexed word in line: {line}".format(**locals()),
+                        end="")
+                    unindexed += 1
             
             print("Translated to {output_path}".format(**locals()))
+            
+    return (unindexed, bad_partition)
 
 if __name__ == '__main__':
     # Define and parse arguments
@@ -62,6 +83,8 @@ if __name__ == '__main__':
     )
     parser.add_argument("ngrams", help="JSON file listing all the ngram files")
     parser.add_argument("index", type=argparse.FileType('r'), help="index file")
+    parser.add_argument("index_ranges",
+        help="JSON file listing first word index ranges for each partition")
     parser.add_argument("input", help="input directory of ngram files")
     parser.add_argument("output", help="output directory of ngram files")
     parser.add_argument("--processes", metavar="P", type=int, default=1,
@@ -71,16 +94,36 @@ if __name__ == '__main__':
     # Read the index of words
     index = read_index(args.index)
     args.index.close()
+    
+    # Load index ranges
+    with open(args.index_ranges, "r") as f:
+        index_ranges = json.load(f)
+    partitions = frozenset(index_ranges.keys())
 
     # Create a generator yielding ngram descriptions
     ngram_descriptions = gen_ngram_descriptions(args.ngrams)
     
     # Process the files
     if args.processes == 1:
+        total_unindexed = 0
+        total_bad_partition = 0
+    
         for ngram in ngram_descriptions:
-            process_file(ngram)
+            (unindexed, bad_partition) = process_file(ngram)
+            total_unindexed += unindexed
+            total_bad_partition += bad_partition
     else:
         p = multiprocessing.Pool(processes=args.processes)
-        p.map(process_file, ngram_descriptions, 1)
+        errors = p.map(process_file, ngram_descriptions, 1)
         p.close()
         p.join()
+
+        (total_unindexed, total_bad_partition) = functools.reduce(
+                lambda x, y: (x[0]+y[0], x[1]+y[1]),
+                errors
+            )
+            
+    print("Ngrams with unindexed words discarded: {total_unindexed}".format(
+        **locals()))
+    print("Ngrams in bad partitions discarded: {total_bad_partition}".format(
+        **locals()))
