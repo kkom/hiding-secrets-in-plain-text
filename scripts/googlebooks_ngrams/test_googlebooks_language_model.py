b4d5019c3b4a1cefe5531ddf246544227375b8e8
Konrad Komorowski <5056119+kkom@users.noreply.github.com>
2014-02-26 21:36:27 +0000

    Add a simple script to test the PostgreSQL google ngrams database.

---
+++ scripts/googlebooks_ngrams/test_googlebooks_language_model.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from sympy import Rational
+
+from pysteg.googlebooks import GooglebooksNgramsLanguageModel
+
+def show(n1, n2):
+    print(float(n1), float(n2))
+
+lm = GooglebooksNgramsLanguageModel("steganography", "googlebooks", 5)
+
+w = 4292446
+context = (4599888,6835529,2722355,6835529)
+
+c1, c2 = lm.c(w, context)
+
+show(c1,c2)
+
+new_c1 = c1 + Rational(1,4) * (c2-c1)
+new_c2 = c2
+
+bad_c2 = c1 + Rational(5,4) * (c2-c1)
+
+show(new_c1,new_c2)
+print(lm.next((new_c1, new_c2), context))
+
+show(new_c1,bad_c2)
+print(lm.next((new_c1, bad_c2), context))
