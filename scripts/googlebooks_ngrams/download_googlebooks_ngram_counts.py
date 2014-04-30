b28f044290963e365be48206ef92ee7b1463709d
Konrad Komorowski <konrad@komorowski.me>
2014-04-30 15:51:05 +0100

    1. Move the POS-tag check back to the function integrating untagged ngram counts.
    2. Clean up the download_googlebooks_ngram_counts script to for example use the DONE wrapper.

--- scripts/googlebooks_ngrams/download_googlebooks_ngram_counts.py
+++ scripts/googlebooks_ngrams/download_googlebooks_ngram_counts.py
@@ -17,82 +17,88 @@ import os
 import time
 import urllib.parse
 
+from pysteg.common.files import open_file_to_process, FileAlreadyProcessed
+
 from pysteg.common.streaming import iter_remote_gzip
 from pysteg.common.streaming import ngrams_iter2file
 
-from pysteg.googlebooks_ngrams.ngrams_analysis import extract_ngram_counts
 from pysteg.googlebooks_ngrams.ngrams_analysis import gen_ngram_descriptions
-
-# Define and parse the script arguments
-parser = argparse.ArgumentParser(description=descr, epilog=epilog)
-
-parser.add_argument("ngrams", help="JSON file listing all the ngram files")
-parser.add_argument("output", help="output directory for processed files")
-parser.add_argument("--processes", metavar="P", type=int, default=1,
-    help="set the number of parallel worker processes (default 1)")
-parser.add_argument("--hours_off", nargs=2, metavar=("START","END"),
-    help=("stop the script from dispatching jobs between two hours (HH:MM "
-          "format)"))
-parser.add_argument("--days_on", metavar="DAY", type=int, nargs='+',
-    help=("force the script to run on the specified days regardless of the "
-          "hours_off parameter (days are numbered starting from Monday as 1)"))
-    
-args = parser.parse_args()
+from pysteg.googlebooks_ngrams.ngrams_analysis import integrate_pure_ngram_counts
+from pysteg.googlebooks_ngrams.ngrams_analysis import ngram_filename
 
 def allowed_to_dispatch():
     """Check if the script is allowed to dispatch another job to the pool."""
-    
+
     if args.days_on:
         if datetime.date.today().isoweekday() in args.days_on:
             return True
-        
+
     if args.hours_off:
         start = datetime.time(*map(int, args.hours_off[0].split(":")))
         end = datetime.time(*map(int, args.hours_off[1].split(":")))
-        
+
         now = datetime.datetime.now()
         time_now = datetime.time(now.hour, now.minute)
-    
+
         if time_now > start and time_now < end:
             return False
         else:
             return True
-            
+
     return True
 
 def process_file(descr):
     """Process a single file."""
-    
+
     while not allowed_to_dispatch():
         time.sleep(300)
-    
-    filename_template = "googlebooks-eng-us-all-{n}gram-20120701-{prefix}"
+
+    n, prefix = descr
+
     local_root = args.output
     remote_root = "http://storage.googleapis.com/books/ngrams/books/"
-    
-    n, prefix = descr
-    
-    filename = filename_template.format(n=n, prefix=prefix)
+
+    filename = ngram_filename(n, prefix)
     local_path = os.path.join(local_root, filename)
     remote_path = urllib.parse.urljoin(remote_root, filename + ".gz")
-    
-    if os.path.isfile(local_path + "_DONE"):
-        print("{t} Skipped {f}".format(t=datetime.datetime.now(),f=filename))
-    else:
-        print("{t} Processing {f}".format(t=datetime.datetime.now(),f=filename))
-        
+
+    def print_status(message, filename):
+        time = datetime.datetime.now()
+        print("{time} {message} {filename}".format(**locals()))
+
+    with open_file_to_process(local_path, "wb") as f:
+        if f == False:
+            print_status("Skipped", filename)
+            raise FileAlreadyProcessed()
+
+        print_status("Processing", filename)
+
         # Generate iterators over ngrams
         source_ngrams = iter_remote_gzip(remote_path)
-        processed_ngrams = extract_ngram_counts(source_ngrams, int(n))
-        
-        with open(local_path, 'wb') as f:
-            ngrams_iter2file(processed_ngrams, f)
-            
-        open(local_path + "_DONE", 'w').close()
-        
-        print("{t} Finished {f}".format(t=datetime.datetime.now(),f=filename))
+        processed_ngrams = integrate_pure_ngram_counts(source_ngrams, int(n))
+
+        # Save the integrated ngram counts to a file
+        ngrams_iter2file(processed_ngrams, f)
+
+        print_status("Finished", filename)
 
 if __name__ == '__main__':
+    # Define and parse the script arguments
+    parser = argparse.ArgumentParser(description=descr, epilog=epilog)
+    parser.add_argument("ngrams",
+        help="JSON file listing all the ngram file descriptions")
+    parser.add_argument("output", help="output directory for processed files")
+    parser.add_argument("--processes", metavar="P", type=int, default=1,
+        help="set the number of parallel worker processes (default 1)")
+    parser.add_argument("--hours_off", nargs=2, metavar=("START","END"),
+        help=("stop the script from dispatching jobs between two hours (HH:MM "
+              "format)"))
+    parser.add_argument("--days_on", metavar="DAY", type=int, nargs='+',
+        help=("force the script to run on the specified days regardless of the "
+              "hours_off parameter (days are numbered starting from Monday as "
+              "1)"))
+    args = parser.parse_args()
+
     ngram_descriptions = gen_ngram_descriptions(args.ngrams)
 
     if args.processes == 1:
