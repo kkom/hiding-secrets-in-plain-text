82028b26b80ced10c754e10d7b37a21a28bacb81
Konrad Komorowski <5056119+kkom@users.noreply.github.com>
2014-04-30 15:51:05 +0100

    1. Move the POS-tag check back to the function integrating untagged ngram counts.
    2. Clean up the download_googlebooks_ngram_counts script to for example use the DONE wrapper.

--- pysteg/googlebooks2.py
+++ pysteg/googlebooks2.py
@@ -1,22 +1,4 @@
-#!/usr/bin/env python3
-
-import re
 import string
-
-from itertools import count, chain
-from functools import lru_cache
 
 PARTITION_NAMES = tuple(string.digits + "_" + string.ascii_lowercase)
 SPECIAL_PREFIXES = frozenset({"other", "punctuation"})
-        
-def pos_tagged(word):
-    """Returns if a word either is or contains a POS tag."""
-
-    # A part-of-speech (POS) tag can either be appended to the end of a word, as
-    # in "eat_VERB", or can be a placeholder on its own, as in "_VERB_".
-    pos_pattern = re.compile("_[A-Z.]+_?$")
-    
-    for potential_tag in pos_pattern.findall(word.decode()):
-        if potential_tag.strip("_") in POS_TAGS:
-            return False
-    return True
--- pysteg/googlebooks_ngrams/ngrams_analysis.py
+++ pysteg/googlebooks_ngrams/ngrams_analysis.py
@@ -1,54 +1,69 @@
 import json
+import re
 
-from pysteg.googlebooks2 import pos_tagged
-
-def extract_ngram_counts(source_ngrams, n):
+def integrate_pure_ngram_counts(source_ngrams, n):
     """
-    Given an iterator over the lines of a Google Books Ngram corpus file returns
+    Given an iterator over the lines of a Google Books Ngram corpus file return
     an iterator over the counts of particular ngrams. The counts are integrated
     over all years and ngrams containing syntactic annotations are discarded.
-    
+
     The function assumes that all lines which have the same ngrams and differ
     only by the year of publication will be adjacent in the file. This
     assumption is needed to generate correct output and is critical to the
     function's performance.
     """
-    
+
+    # A part-of-speech (POS) tag can either be appended to the end of a word, as
+    # in "eat_VERB", or can be a placeholder on its own, as in "_VERB_".
+    pos_pattern = re.compile("_[A-Z.]+_?$")
+    pos_tags = frozenset({"NOUN", "VERB", "ADJ", "ADV", "PRON", "DET", "ADP",
+        "NUM", "CONJ", "PRT", ".", "X"})
+
+    def pos_tagged(word):
+        """
+        Return if a word (represented by a Bytes array) either is or contains a
+        POS tag.
+        """
+        for potential_tag in pos_pattern.findall(word.decode()):
+            if potential_tag.strip("_") in pos_tags:
+                return True
+        return False
+
     def valid_ngram(ngram):
         """Check if the current ngram is valid, i.e. POS tag-free."""
         for word in ngram:
             if pos_tagged(word):
                 return False
         return True
-    
+
     current_ngram = []
     current_ngram_count = 0
     current_ngram_valid = False
-    
+
     for line in source_ngrams:
         data = line.split()
-        
+
         if data[0:n] == current_ngram:
             current_ngram_count += int(data[n+1])
         else:
             if current_ngram_valid:
                 yield (tuple(current_ngram), current_ngram_count)
-            
+
             current_ngram = data[0:n]
             current_ngram_count = int(data[n+1])
             current_ngram_valid = valid_ngram(current_ngram)
 
 def gen_ngram_descriptions(filename):
     """Yields ngram descriptions from a file."""
-    
+
     with open(filename, 'r') as f:
         ngrams = json.load(f)
-        
+
     for n in sorted(ngrams.keys()):
         for prefix in ngrams[n]:
             yield (n, prefix)
-            
-def ngram_filename(n,prefix):
-    """Returns a standard ngram filename from its order and prefix."""
-    
+
+def ngram_filename(n, prefix):
+    """Return a standard ngram filename from its order and prefix."""
+
     return "googlebooks-eng-us-all-{n}gram-20120701-{prefix}".format(**locals())
