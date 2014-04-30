652f44ed7fea2870a6696304a62e1fe2d6443437
Konrad Komorowski <5056119+kkom@users.noreply.github.com>
2014-04-30 01:16:39 +0100

    Clean up and comment the topological string sort function. Also make the 'prefix comparison' be a strictly prefix rather than substring comparison.

--- pysteg/common/graphs.py
+++ pysteg/common/graphs.py
@@ -1,18 +1,31 @@
-def topological_string_sort(l_unsorted):
+def topological_string_sort(lst_unsorted):
     """Sorts a list of strings so that any prefix of a string comes after it."""
     
+    def is_prefix(a,b):
+        """Returns whether string a is a prefix of string b."""
+        return len(a) <= len(b) and b[:len(a)] == a
+    
     def visit(n):
+        """
+        First goes through all the other nodes to look for nodes of which n is a
+        prefix. If any such node is found, it is visited recursively. Finally,
+        after all topologically preceding nodes are visited, node n is added to
+        the sorted list. 
+        """
         unseen.remove(n)
         
-        for m in range(len(l)):
-            if n != m and l[n] in l[m] and m in unseen:
+        # Iterate over all nodes and for the ones that haven't been seen yet
+        # check if they should be placed before n. Note that there is no need to
+        # discard comparisons with n itself, as it was "seen" in the line above.
+        for m in range(len(lst)):
+            if m in unseen and is_prefix(lst[n],lst[m]):
                 visit(m)
         
-        l_sorted.append(l[n])
+        lst_sorted.append(lst[n])
     
-    l = tuple(l_unsorted)
-    l_sorted = []
-    unseen = set(range(len(l)))
+    lst = tuple(lst_unsorted)
+    lst_sorted = []
+    unseen = set(range(len(lst)))
     
     while len(unseen) > 0:
         # The following two lines of code are here only because there is no
@@ -21,4 +34,4 @@ def topological_string_sort(l_unsorted):
         unseen.add(n)
         visit(n)
         
-    return l_sorted
+    return lst_sorted
