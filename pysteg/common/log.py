f98168e0da8dfd837b53a5468859e4db953e9240
Konrad Komorowski <5056119+kkom@users.noreply.github.com>
2014-05-03 14:58:14 +0100

    Add a package used for outputting log information.

---
+++ pysteg/common/log.py
@@ -0,0 +1,7 @@
+import datetime
+
+def print_status(*args):
+    """Output timestamp and space concatenated arguments to the terminal."""
+
+    timestamp = str(datetime.datetime.now())
+    print(" ".join((timestamp,) + args))
