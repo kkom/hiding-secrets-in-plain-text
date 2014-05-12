3fcbc138f2a0df4a7d4bcdc450c0915220b079ac
Konrad Komorowski <5056119+kkom@users.noreply.github.com>
2014-05-12 21:21:11 +0200

    Use a more consistent name for the next function in rational AC decoding.

--- pysteg/coding/rational_ac.py
+++ pysteg/coding/rational_ac.py
@@ -1,12 +1,12 @@
-def decode(next_fun, interval):
+def decode(next, interval):
     """Decode an interval using the supplied next token function."""
 
     context = []
-    search_result = next_fun(interval, tuple(context))
+    search_result = next(interval, tuple(context))
 
     while search_result is not None:
         print(search_result[0])
         context.append(search_result[0])
-        search_result = next_fun(search_result[1], tuple(context))
+        search_result = next(search_result[1], tuple(context))
 
     return context
