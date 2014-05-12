9185b80c33b9d6fa7febb64a701ade41a4a2dd13
Konrad Komorowski <konrad@komorowski.me>
2014-05-12 17:51:21 +0200

    Add function allowing to go between rational intervals and binary sequences.

--- pysteg/coding/interval.py
+++ pysteg/coding/interval.py
@@ -1,8 +1,67 @@
 import collections
+import functools
 
 import sympy
 
 Interval = collections.namedtuple("Interval", "b l")
+
+__half = sympy.Rational(1,2)
+__first_half = create_interval(sympy.Rational(0), sympy.Rational(1,2))
+__second_half = create_interval(sympy.Rational(1,2), sympy.Rational(1,2))
+
+def bit2interval(bit):
+    """Convert 0 to the [0,0.5) interval and 1 to the [0.5,1) interval."""
+
+    if bit == 0:
+        return __first_half
+    else:
+        return __second_half
+
+def bits2interval(bits):
+    """Convert a sequence of bits to the interval they describe."""
+    return functools.reduce(find_subinterval, map(bit2interval, bits))
+
+def interval2bit(interval, mode):
+    assert(mode in ("sub", "super"))
+
+    if mode == "super":
+        # The order of the first two conditions matters. Since the interval is
+        # closed at start and open at the end, a zero-length interval at 1/2
+        # should be matched to 1.
+        if interval.b >= __half:
+            return (1, scale_interval(__second_half, interval))
+        elif interval.b + interval.l <= __half:
+            return (0, scale_interval(__first_half, interval))
+        else:
+            return None
+    else:
+        bottom_distance = interval.b
+        top_distance = 1 - (interval.b + interval.l)
+
+        if bottom_distance <= 0 and top_distance <= 0:
+            return None
+        if top_distance < bottom_distance:
+            return (1, scale_interval(__second_half, interval, subunit=False))
+        else:
+            return (0, scale_interval(__first_half, interval, subunit=False))
+
+def interval2bits(interval, mode):
+    """
+    Convert an interval to a sequence of bits that describe its smallest
+    superinterval or largest subinterval.
+    """
+
+    assert(mode in ("sub", "super"))
+
+    bits = []
+
+    next = interval2bit(interval, mode)
+    while next is not None:
+        bits.append(next[0])
+        interval = next[1]
+        next = interval2bit(interval, mode)
+
+    return tuple(bits)
 
 def create_interval(base, length, divisor=1, subunit=True):
     b = sympy.Rational(base, divisor)     # interval base
