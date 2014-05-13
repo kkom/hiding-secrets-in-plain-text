fa6b1fb542355f540c5600865fa96fc6e20f59af
Konrad Komorowski <konrad@komorowski.me>
2014-05-13 17:21:53 +0200

    Improve comments on the interval module.

--- pysteg/coding/interval.py
+++ pysteg/coding/interval.py
@@ -7,12 +7,17 @@ from pysteg.coding.crypto import random_bits
 
 Interval = collections.namedtuple("Interval", "b l")
 
+# The rational number 1/2
 __half = sympy.Rational(1,2)
+
+# Half-unit intervals - [0, 1/2) for 0 and [1/2, 1/2) for 1
 __first_half = Interval(sympy.Rational(0), sympy.Rational(1,2))
 __second_half = Interval(sympy.Rational(1,2), sympy.Rational(1,2))
 
 def bit2interval(bit):
-    """Convert 0 to the [0,0.5) interval and 1 to the [0.5,1) interval."""
+    """Convert 0 and 1 to their half-unit intervals."""
+
+    assert(bit in (0,1))
 
     if bit == 0:
         return __first_half
@@ -24,12 +29,23 @@ def bits2interval(bits):
     return functools.reduce(find_subinterval, map(bit2interval, bits))
 
 def interval2bit(interval, mode):
+    """
+    In "super" mode, return bit corresponding to the half-unit interval which is
+    a superinterval of the input interval. Additionally, return as a second
+    element of the tuple ratio of the input interval to the chosen half-unit
+    interval. Return None if no half-unit interval satisfies this requirement.
+
+    In "sub" mode, return whichever half-unit interval contains more of the
+    input interval and the same ratio. Return None if the input interval is a
+    superinterval of [0,1).
+    """
+
     assert(mode in ("sub", "super"))
 
     if mode == "super":
-        # The order of the first two conditions matters. Since the interval is
-        # closed at start and open at the end, a zero-length interval at 1/2
-        # should be matched to 1.
+        # Since all intervals are closed at start and open at the end, [1/2, 0)
+        # i.e. a point at 1/2 should be matched to the [1/2, 1/2) interval. This
+        # is why the order of the conditions matters.
         if interval.b >= __half:
             return (1, scale_interval(__second_half, interval))
         elif interval.b + interval.l <= __half:
@@ -66,14 +82,20 @@ def interval2bits(interval, mode):
     return tuple(bits)
 
 def create_interval(base, length, divisor=1, subunit=True):
+    """
+    Create an interval. Make sure that its length is positive. In most cases the
+    interval should be a sub-unit interval, i.e. a subinterval of [0,1). This is
+    enforced by default.
+    """
+
     b = sympy.Rational(base, divisor)     # interval base
     l = sympy.Rational(length, divisor)   # interval length
 
     # Interval has to have a positive length
     assert(l > 0)
 
+    # And be a subinterval of [0,1)
     if subunit:
-        # And be a subinterval of [0,1)
         assert(b >= 0)
         assert(b < 1)
         assert(b+l <= 1)
@@ -81,7 +103,7 @@ def create_interval(base, length, divisor=1, subunit=True):
     return Interval(b, l)
 
 def find_subinterval(interval, ratio, subunit=True):
-    """Return a subinterval of the interval according to the supplied ratio."""
+    """Return a subinterval of the given interval according to the ratio."""
 
     return create_interval(
         interval.b + ratio.b * interval.l,
@@ -98,7 +120,7 @@ def is_subinterval(interval, subinterval, proper=False):
                 subinterval.b + subinterval.l <= interval.b + interval.l)
 
 def random_interval(n, seed=None):
-    """Randomly generate an interval of n bits."""
+    """Randomly generate a sub-unit interval corresponding to n bits."""
     return bits2interval(random_bits(n, seed))
 
 def scale_interval(superinterval, interval, subunit=True):
