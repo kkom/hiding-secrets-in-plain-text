46eea93ab07cb80036156dbda1104555a8a3a3c5
Konrad Komorowski <5056119+kkom@users.noreply.github.com>
2014-05-15 00:45:51 +0100

    Implement limiting the number of iterations of convergence function.

--- pysteg/common/functools.py
+++ pysteg/common/functools.py
@@ -1,8 +1,11 @@
-def converge(f, x, limit=0):
+def converge(f, x, limit=float("inf")):
     """Repeatedly apply f to x until the result does not change."""
 
     fx = f(x)
-    while fx != x:
+    i = limit-1
+
+    while fx != x and i > 0:
         x = fx
         fx = f(x)
+        i -= 1
     return fx
